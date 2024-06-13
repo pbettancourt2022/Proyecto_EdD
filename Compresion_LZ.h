f699a0867f89c751a9e87a1cc09f1f6b0ed9daa9
SonodaPopin <joahernandez2022@udec.cl>
2024-06-12 23:54:57 -0400

    Arreglo parcial de la funcion de compresion lz, y adicion de un texto de 4,4MB con el que haacer pruebas

--- Compresion_LZ.h
+++ Compresion_LZ.h
@@ -4,100 +4,63 @@
 #include <unordered_map>
 #include <memory>
 
-// Nodo del Trie
-struct TrieNode {
-    std::unordered_map<char, std::unique_ptr<TrieNode>> children;
-    int position;
 
-    TrieNode() : position(-1) {}
-};
-
-class Trie {
-private:
-    std::unique_ptr<TrieNode> root;
-
-public:
-    Trie() : root(std::make_unique<TrieNode>()) {}
-
-    void insert(const std::string& str, int pos) {
-        TrieNode* node = root.get();
-        for (char c : str) {
-            if (!node->children[c]) {
-                node->children[c] = std::make_unique<TrieNode>();
-            }
-            node = node->children[c].get();
-        }
-        node->position = pos;
-    }
-
-    std::pair<int, int> searchLongestPrefix(const std::string& str, int start) {
-        TrieNode* node = root.get();
-        int maxLength = 0;
-        int position = -1;
-        for (int i = start; i < str.size(); ++i) {
-            if (!node->children[str[i]]) {
-                break;
-            }
-            node = node->children[str[i]].get();
-            maxLength = i - start + 1;
-            position = node->position;
-        }
-        return {position, maxLength};
-    }
-};
-
-// Estructura para almacenar los pares (posición, longitud)
 struct Pair {
     int position;
     int length;
     char nextChar;
 
-    Pair(int pos, int len, char next) : position(pos), length(len), nextChar(next) {}
+    Pair(int p, int l, char c) : position(p), length(l), nextChar(c) {}
 };
 
-// Clase para la compresión y descompresión LZ
 class LZCompression {
 public:
-    // Método para comprimir el texto
     std::vector<Pair> comprimir(const std::string& text) {
         std::vector<Pair> compressed;
-        Trie trie;
-
+        std::unordered_map<std::string, int> dictionary;
         int i = 0;
+
         while (i < text.size()) {
-            int length = 0;
-            int position = -1;
-            char nextChar = text[i];
+            int maxLength = 0;
+            int maxPos = -1;
+            char nextChar = '\0';
 
-            auto [foundPosition, foundLength] = trie.searchLongestPrefix(text, i);
-
-            if (foundPosition != -1) {
-                position = foundPosition;
-                length = foundLength;
-                if (i + length < text.size()) {
-                    nextChar = text[i + length];
+            for (int length = 1; length <= text.size() - i; ++length) {
+                std::string substring = text.substr(i, length);
+                auto it = dictionary.find(substring);
+                if (it != dictionary.end()) {
+                    maxLength = length;
+                    maxPos = it->second;
+                } else {
+                    break;
                 }
             }
 
-            compressed.push_back(Pair(position, length, nextChar));
-
-            // Insertar el substring actual en el trie
-            std::string currentSubstring = text.substr(i, length + 1);
-            trie.insert(currentSubstring, i);
-
-            i += length + 1;
+            if (maxLength > 0) {
+                if (i + maxLength < text.size()) {
+                    nextChar = text[i + maxLength];
+                }
+                compressed.push_back(Pair(i - maxPos, maxLength, nextChar));
+                dictionary[text.substr(i, maxLength + 1)] = i;
+                i += maxLength + 1;
+            } else {
+                compressed.push_back(Pair(-1, 0, text[i]));
+                dictionary[text.substr(i, 1)] = i;
+                ++i;
+            }
         }
+
         return compressed;
     }
 
-    // Método para descomprimir la secuencia de pares
     std::string descomprimir(const std::vector<Pair>& compressed) {
         std::string decompressed;
 
         for (const auto& p : compressed) {
             if (p.position != -1) {
+                int start = decompressed.size() - p.position;
                 for (int i = 0; i < p.length; ++i) {
-                    decompressed += decompressed[p.position + i];
+                    decompressed += decompressed[start + i];
                 }
             }
             if (p.nextChar != '\0') {
@@ -108,4 +71,3 @@ public:
         return decompressed;
     }
 };
-
